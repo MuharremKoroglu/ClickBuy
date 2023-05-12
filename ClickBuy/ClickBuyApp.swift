21753840beb4307d01aa3c27dca6b6d590480a6b
Muharrem Koroglu <muharrem-koroglu.99@hotmail.com>
2023-05-12 10:26:54 +0300

    Initial Commit

---
+++ ClickBuy/ClickBuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickBuyApp.swift
+//  ClickBuy
+//
+//  Created by Muharrem Köroğlu on 12.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ClickBuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
